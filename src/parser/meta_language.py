ca4efa33922a0ba35312e0c63ca2463393eb1eb7
Michał Szopiński <mszopinski@mssnt.pl>
2022-05-22 20:14:52 +0200

    Recreate metagrammar

--- src/parser/meta_language.py
+++ src/parser/meta_language.py
@@ -2,13 +2,78 @@ from parser.productions import Terminal, Concatenation, Optional, Alternative
 
 # define grammar for the grammar definition file
 meta_grammar = {
-    "root": Concatenation(
-        Optional(
-            Terminal("identifier", "alpha")
-        ),
+    "root": Optional("definitions"),
+    "definitions": Concatenation(
+        "definition",
+        Optional("definitions")
+    ),
+    "definition": Concatenation(
+        "definition_key",
         Concatenation(
-            Terminal("identifier", "alpha"),
-            Terminal("identifier", "beta")
+            Terminal("auxillary", "="),
+            Concatenation(
+                "definition_value",
+                Terminal("auxillary", ";")
+            )
         )
-    )
+    ),
+    "definition_key": Terminal("identifier"),
+    "definition_value": "expression",
+    "expression": Alternative(
+        Alternative(
+            Terminal("identifier"),
+            Alternative(
+                Terminal("string_literal"),
+                Terminal("identifier", "identifier")
+            )
+        ),
+        Alternative(
+            Terminal("identifier", "string_literal"),
+            Alternative(
+                Terminal("identifier", "number_literal"),
+                "operator"
+            )
+        )
+    ),
+    "operator": Alternative(
+        "opt_operator",
+        Alternative(
+            "concat_operator",
+            "alt_operator"
+        )
+    ),
+    "opt_operator": Concatenation(
+        Terminal("identifier", "opt"),
+        "single_arg"
+    ),
+    "concat_operator": Concatenation(
+        Terminal("identifier", "concat"),
+        "double_arg"
+    ),
+    "alt_operator": Concatenation(
+        Terminal("identifier", "alt"),
+        "double_arg"
+    ),
+    "single_arg": Concatenation(
+        Terminal("auxillary", "("),
+        Concatenation(
+            "arg1",
+            Terminal("auxillary", ")"),
+        )
+    ),
+    "double_arg": Concatenation(
+        Terminal("auxillary", "("),
+        Concatenation(
+            "arg1",
+            Concatenation(
+                Terminal("auxillary", ","),
+                Concatenation(
+                    "arg2",
+                    Terminal("auxillary", ")")
+                )
+            )
+        )
+    ),
+    "arg1": "expression",
+    "arg2": "expression"
 }
