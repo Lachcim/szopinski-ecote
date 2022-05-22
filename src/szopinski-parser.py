da3d7adf4b851d0c148fd7fa37e2bc727bef5451
Michał Szopiński <mszopinski@mssnt.pl>
2022-05-22 22:18:13 +0200

    Refactor main script

--- src/szopinski-parser.py
+++ src/szopinski-parser.py
@@ -5,6 +5,12 @@ from parser.diagnostic import print_parser_error, print_tree
 from parser.parser import Parser, ParseError
 from parser.meta_language import meta_grammar
 
+def read_file_contents(path):
+    with open(path, "r") as f:
+        file_contents = f.read()
+        file_contents = file_contents.replace("\t", "    ")
+        return file_contents
+
 # print usage information
 if len(sys.argv) < 3:
     print("Usage: python preliminary_parser.py [grammar file] [input file]")
@@ -12,18 +18,12 @@ if len(sys.argv) < 3:
 
 # read files
 grammar_file_path = sys.argv[1]
+grammar_file_raw = read_file_contents(grammar_file_path)
 input_file_path = sys.argv[2]
-
-with open(grammar_file_path, "r") as f:
-    grammar_file_raw = f.read()
-    grammar_file_raw = grammar_file_raw.replace("\t", "    ")
-with open(input_file_path, "r") as f:
-    input_file_raw = f.read().replace("\t", "    ")
-    input_file_raw = input_file_raw.replace("\t", "    ")
+input_file_raw = read_file_contents(input_file_path)
 
 # tokenize grammar file
 grammar_tokens = scan_and_evaluate(grammar_file_raw)
-
 if has_lexer_errors(grammar_tokens):
     print_lexer_errors(grammar_tokens, grammar_file_raw, grammar_file_path)
     exit(1)
@@ -32,8 +32,8 @@ try:
     # parse grammar file using grammar definition metalanguage
     grammar_parser = Parser(grammar_tokens, meta_grammar)
     grammar_parser.parse()
-except ParseError as e:
-    print_parser_error(e, grammar_file_raw, grammar_file_path)
+except ParseError as error:
+    print_parser_error(error, grammar_file_raw, grammar_file_path)
     exit(1)
 
 print_tree(grammar_parser.super_root)
